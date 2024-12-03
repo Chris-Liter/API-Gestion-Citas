7f413bd2774ff405ff7df02fcbdfaf907397fc41
Jorge Lituma <jorgelituma096@gmail.com>
2024-12-03 17:21:33 -0500

    primer

---
+++ src/test/java/com/sistema/citas/ups/UpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.citas.ups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
